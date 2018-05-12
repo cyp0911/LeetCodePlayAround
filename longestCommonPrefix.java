20b825a4f736fe2784081f57fa1c7be5bccdfdfd
Yinpeng Chen <cyp0911@gmail.com>
2018-05-11 23:47:39 -0300

    8th

---
+++ longestCommonPrefix.java
@@ -0,0 +1,33 @@
+class longestCommonPrefix{
+	
+	public static String commom(String[] test)
+	{
+		if (test.length == 0) return "";
+		String prefix = test[0];
+		
+		for (int i = 0; i < test.length; i++)
+		{
+			while(test[i].indexOf(prefix) != 0)
+			{
+				prefix = prefix.substring(0, prefix.length() - 1);
+				if (prefix.isEmpty()) return "";
+			}
+		}
+		
+		
+		
+		return prefix;
+	}
+	
+	public static void main(String args[])
+	{
+		String[] test = {"asdf", "as", "asb", "askeke"};
+		String prefix = "";
+		
+		prefix = commom(test);
+		System.out.println(prefix);
+	}
+	
+	
+	
+}
